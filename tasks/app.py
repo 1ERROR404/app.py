e500bdeb4d734561b811b94a67d055f5018807cb
1ERROR404 <49617439+1ERROR404@users.noreply.github.com>
2022-12-28 14:00:27 +0400

    Add files via upload

---
+++ tasks/app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://postgres@0.0.0.0:8089/oman'
+
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+    __tablename__= 'Persons'
+    id =db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+
+db.create_all()
+
+@app.route('/')
+def index():
+    with app.app_context():
+        return "Hello i am back"
+
+
+
+
+
+if __name__=='__main__':
+    app.debug = True
+    app.run(host="0.0.0.0", port=8089)
